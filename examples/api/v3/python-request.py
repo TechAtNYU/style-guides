eb19eea7c4c06da5923d884bd6d1d7a36716d6ed
Abhi Agarwal <me@abhiagarwal.com>
2015-10-30 17:17:57 -0400

    The usual stupid mistake

--- examples/api/v3/python-request.py
+++ examples/api/v3/python-request.py
@@ -3,7 +3,7 @@
 
 import requests, json, os
 
-headers = {'content-type': 'application/vnd.api+json', 'accept': 'application/*, text/*', 'authorization': ('Bearer', os.environ['TNYU_API_KEY'])}
+headers = {'content-type': 'application/vnd.api+json', 'accept': 'application/*, text/*', 'authorization': 'Bearer ' + os.environ['TNYU_API_KEY']}
 r = requests.get('https://api.tnyu.org/v3/events', headers=headers, verify=False)
 data = json.loads(r.text)
 print data
