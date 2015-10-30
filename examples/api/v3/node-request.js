4c0adbf08de3bd518e13078d2d30ad52fe242396
Abhi Agarwal <me@abhiagarwal.com>
2015-10-30 19:36:13 -0400

    Adding node v3 example

--- examples/api/v3/node-request.js
+++ examples/api/v3/node-request.js
@@ -6,11 +6,11 @@ request({
   //this disables the ssl security (would accept a fake certificate). see:
   //http://stackoverflow.com/questions/20082893/unable-to-verify-leaf-signature
   "rejectUnauthorized": false,
-  "url": 'https://api.tnyu.org/v2/people',
+  "url": 'https://api.tnyu.org/v3/people',
   "headers": {
     "content-type": "application/vnd.api+json",
     "accept": "application/*, text/html, */*",
-    "x-api-key": process.env.TNYU_API_KEY
+    "authorization": "Bearer " + process.env.TNYU_API_KEY
   },
   timeout: 100000
 }, function(err, response, body){
