4341a65c390659c422850ed6894ed1adab004aaf
Abhi Agarwal <me@abhiagarwal.com>
2016-01-08 20:13:43 +0700

    Update python-request.py

--- examples/api/v3/python-request.py
+++ examples/api/v3/python-request.py
@@ -1,9 +1,17 @@
 # These two commands solve the issue of HTTPS InsecurePlatformWarning in Python:
 # pip install requests
 
-import requests, json, os
+import requests
+import json
+import os
 
-headers = {'content-type': 'application/vnd.api+json', 'accept': 'application/*, text/*', 'authorization': 'Bearer ' + os.environ['TNYU_API_KEY']}
-r = requests.get('https://api.tnyu.org/v3/events', headers=headers, verify=False)
+headers = {
+    'content-type': 'application/vnd.api+json',
+    'accept': 'application/*, text/*',
+    'authorization': 'Bearer ' + os.environ['TNYU_API_KEY']
+}
+
+r = requests.get('https://api.tnyu.org/v3/events',
+                 headers=headers, verify=False)
 data = json.loads(r.text)
 print data
