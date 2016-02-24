11d6d8b033e5a8ebdfc2a2291dfad31219f78ed6
Abhi Agarwal <me@abhiagarwal.com>
2016-02-24 16:05:53 -0500

    Adding ability to export RSVPs to PDF in preperation for Microsoft event

---
+++ scripts/rsvps_to_pdf.py
@@ -0,0 +1,43 @@
+# These two commands solve the issue of HTTPS InsecurePlatformWarning in Python:
+# pip install requests
+
+import requests
+import json
+import os
+from TableFactory import *
+
+BASE_URL = 'https://api.tnyu.org/v3'
+TEST_BASE_URL = 'https://api.tnyu.org/v3-test'
+
+EVENT_ID = '56c29c57e7afddface1d78c8'
+
+headers = {
+    'content-type': 'application/vnd.api+json',
+    'accept': 'application/*, text/*',
+    'authorization': 'Bearer ' + os.environ['TNYU_API_KEY_ADMIN']
+}
+
+# Reading data from the API
+r = requests.get(BASE_URL + '/events/' + EVENT_ID + '?include=rsvps',
+                 headers=headers, verify=False)
+data = json.loads(r.text)
+event = data['data']
+rsvps = data['included']
+
+# Print emails
+rsvp_for_pdf = []
+
+for i in rsvps:
+    to_append = {
+        'name': i['attributes']['name'],
+        'email': i['attributes']['contact']['email']
+    }
+    rsvp_for_pdf.append(to_append)
+
+pdf_title = event['attributes']['title'].replace(' ', '_')
+
+rowmaker = RowSpec(ColumnSpec('name', 'Name'),
+                   ColumnSpec('email', 'Email'))
+lines = rowmaker.makeall(rsvp_for_pdf)
+pdfmaker = PDFTable('Attendees for ' + event['attributes']['title'], headers=rowmaker)
+open(pdf_title + '.pdf', 'wb').write(pdfmaker.render(lines))
