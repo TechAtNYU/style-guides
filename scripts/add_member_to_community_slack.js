07f2dccaff4b0f256e5227499252c736db9f6352
Abhi Agarwal <me@abhiagarwal.com>
2016-02-24 14:18:26 -0500

    Adding script to add people to slack

--- scripts/add_member_to_community_slack.js
+++ scripts/add_member_to_community_slack.js
@@ -2,20 +2,45 @@ var request = require('request');
 
 var EVENT_ID = '56c29c57e7afddface1d78c8';
 
+var addToSlack = function(person) {
+    if (person['attributes'] && person['attributes']['contact'] && person['attributes']['contact']['email']) {
+        request.post({
+            url: 'https://technyucommunity.slack.com/api/users.admin.invite',
+            form: {
+                email: person['attributes']['contact']['email'],
+                token: process.env.TNYU_SLACK_TOKEN,
+                set_active: true,
+                extra_message: "Hi! You signed up for 'Backend Development With Node.js' event through our RSVP platform. We're inviting you to our Tech@NYU community slack that we will be using throughout our event. You'll be the first ones to try out our new platform! You'll be able to ask questions on installation problems before the event or questions during the event if you get stuck! You'll also always have access to the slack so you can ask us questions when you get stuck! Welcome to Tech@NYU!"
+            }
+        }, function(err, httpResponse, body) {
+            if (err) {
+                console.log(err);
+            }
+            if (!body['ok']) {
+                console.log(person['attributes']['name']);
+                console.log(body);
+            }
+        });
+    }
+}
+
 request({
-  //this disables the ssl security (would accept a fake certificate). see:
-  //http://stackoverflow.com/questions/20082893/unable-to-verify-leaf-signature
-  "rejectUnauthorized": false,
-  "url": 'https://api.tnyu.org/v3/events/' + EVENT_ID + '?include=rsvps',
-  "headers": {
-    "content-type": "application/vnd.api+json",
-    "accept": "application/*, text/html, */*",
-    "authorization": "Bearer " + process.env.TNYU_API_KEY
-  },
-  timeout: 100000
-}, function(err, response, body){
+    //this disables the ssl security (would accept a fake certificate). see:
+    //http://stackoverflow.com/questions/20082893/unable-to-verify-leaf-signature
+    "rejectUnauthorized": false,
+    "url": 'https://api.tnyu.org/v3/events/' + EVENT_ID + '?include=rsvps',
+    "headers": {
+        "content-type": "application/vnd.api+json",
+        "accept": "application/*, text/html, */*",
+        "authorization": "Bearer " + process.env.TNYU_API_KEY
+    },
+    timeout: 100000
+}, function(err, response, body) {
     var apiJson = JSON.parse(body),
         event = apiJson["data"],
         rsvps = apiJson["included"];
-    console.log(event); // print out the first events
+
+    rsvps.forEach(function(person) {
+        addToSlack(person);
+    });
 });
